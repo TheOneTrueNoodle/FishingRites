1bb5f96d7e313dc87f25562420984001c90e5432
ctrlcookie <regardingquiet@gmail.com>
2022-10-16 18:41:03 +0100

    Cultist

---
+++ Fishing_Rites/Assets/Scripts/J_Cultist.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_Cultist : MonoBehaviour
+{
+    public GameObject textbox;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        textbox.SetActive(true);
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        textbox.SetActive(false);
+    }
+}
