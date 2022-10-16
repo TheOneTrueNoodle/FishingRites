226b03b02335813c83abde453dece26e2430e4ad
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2022-10-16 18:01:52 +0100

    OMG WAHHHHHHHHHHHHH

--- Fishing_Rites/Assets/Scripts/R_FishMovement.cs
+++ Fishing_Rites/Assets/Scripts/R_FishMovement.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class R_FishMovement : MonoBehaviour
 {
     public float LeftMaxX, RightMaxX;
-    public float MoveSpeed;
+    public float MaxMoveSpeed = 5f;
+    public float MinMoveSpeed = 1f;
+    private float currentMoveSpeed;
 
     private float NextPosX;
     private Vector3 NextPos;
 
     private void Start()
     {
+        currentMoveSpeed = Random.Range(MinMoveSpeed, MaxMoveSpeed);
         NextPosX = Random.Range(LeftMaxX, RightMaxX);
         NextPos = new Vector3(NextPosX, transform.position.y, transform.position.z);
     }
@@ -20,10 +23,11 @@ public class R_FishMovement : MonoBehaviour
     {
         if(transform.position != NextPos)
         {
-            transform.position = Vector2.MoveTowards(transform.position, NextPos, MoveSpeed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, NextPos, currentMoveSpeed * Time.deltaTime);
         }
         else
         {
+            currentMoveSpeed = Random.Range(MinMoveSpeed, MaxMoveSpeed);
             NextPosX = Random.Range(LeftMaxX, RightMaxX);
             NextPos = new Vector3(NextPosX, transform.position.y, transform.position.z);
         }
